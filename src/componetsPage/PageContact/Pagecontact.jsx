54a0e240b6d3633a22166772adb6fd98e00f3ccc
LUISTAPIARUIZ <luistapiaruiz17@gmial.com>
2023-10-31 12:28:56 -0500

    seccion de conctacto terminado Nota(falta tener una mejora adpatacion a mobiles) la seccion de servicios no esta adapatado a mobiles correctamente

--- src/componetsPage/PageContact/Pagecontact.jsx
+++ src/componetsPage/PageContact/Pagecontact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import "./style.css"
 import imgFB from "./img/facebook.png"
 import imgInstram from "./img/instagram.png"
@@ -7,28 +7,46 @@ import imgWhatsapp from "./img/whatsapp.png"
 import BtnEnviar from "../../components/BotonMain/Btn-main.jsx"
 import imgFormGmail from "./img/imgFrormGmail.jpg"
 import imgPersona from "../../components/PersonInfo/img/Persona.jpg"
+import nameInstagram from "./img/NameInstagram.png"
+import nameFaceboock from "./img/NameFaceboock.png"
 
 const Pagecontact = ()=>{ 
+    const [selectedContact, setSelectedContact]=useState(0)
+    const handleSelectectcontact=(position)=>{
+        setSelectedContact(position)
+    }
     return(
         <section className="containerContact">
             <div className="contact-List">
                 <ul className="List-itemContact">
-                    <li className="itemContact">
+                    <li 
+                    className="itemContact" 
+                    onClick={() => handleSelectectcontact(0)} 
+                    style={{ backgroundColor: selectedContact === 0 ? 'rgb(255, 255, 255,0.6)' : 'transparent',}}>
                         <figure>
                             <img src={imgMail} alt="icon gmail" />
                         </figure>
                     </li>
-                    <li className="itemContact">
+                    <li 
+                    className="itemContact" 
+                    onClick={() => handleSelectectcontact(1)}
+                    style={{ backgroundColor: selectedContact === 1 ? 'rgb(255, 255, 255,0.6)' : 'transparent',}}>
                         <figure>
                             <img src={imgWhatsapp} alt="icon whatsapp" />
                         </figure>
                     </li>
-                    <li className="itemContact">
+                    <li 
+                    className="itemContact"
+                    onClick={() => handleSelectectcontact(2)}
+                    style={{ backgroundColor: selectedContact === 2 ? 'rgb(255, 255, 255,0.6)' : 'transparent',}}>
                         <figure>
                             <img src={imgInstram} alt="icon instagram" />
                         </figure>
                     </li>
-                    <li className="itemContact">
+                    <li 
+                    className="itemContact" 
+                    onClick={() => handleSelectectcontact(3)}
+                    style={{ backgroundColor: selectedContact === 3 ? 'rgb(255, 255, 255,0.6)' : 'transparent',}}>
                         <figure>
                             <img src={imgFB} alt="icon faceboock" />
                         </figure>
@@ -36,6 +54,8 @@ const Pagecontact = ()=>{
                 </ul>
             </div>
             <div className="contacts">
+                {selectedContact===0 &&
+                (
                 <div className="contacts-Gmail">
                         <div className="containerImgContact">
                             <figure>
@@ -97,42 +117,78 @@ const Pagecontact = ()=>{
                             </form>
                         </div>
                 </div>
+                )}
+                {selectedContact===1 &&
+                (
                 <div className="contacts-Whatsapp">
                     <div className="whatsapp-info">
-                            <figure>
-                                <img src={imgPersona} alt="" />
+                            <figure className="info-Photo">
+                                <img src={imgPersona} alt="Foto Maricarmen" />
                             </figure>
-                            <div>
-                                <p>Maricarmen Make Up</p>
+                            <div className="whatsapp-info_Name">
+                                <p>Maricarmen Bohórquez Davila</p>
                             </div>
                             <div class="whatsapp-info_radio-group">
                                 <h5>Servicios</h5>
                                 <label>
-                                    Opción 1<input type="radio" name="opcion" value="opcion1"/>
+                                    Novias<input type="radio" name="opcion" value="Novias"/>
                                 </label>
                                 <label>
-                                    Opción 2<input type="radio" name="opcion" value="opcion2"/>
+                                    Evento<input type="radio" name="opcion" value="Evento"/>
                                 </label>
                                 <label>
-                                    Opción 3<input type="radio" name="opcion" value="opcion3"/>
+                                    Catalogo<input type="radio" name="opcion" value="Catalogo"/>
                                 </label>
                                 <label>
-                                    Opción 3<input type="radio" name="opcion" value="opcion4"/>
+                                    Formacion<input type="radio" name="opcion" value="Formacion"/>
                                 </label>
                             </div>
                     </div>
                     <div className="containerBtnWhatsapp">
-                        <div className="whatsapp-btn">
+                        <div className="btnMain">
                             <BtnEnviar class="btn pink font-size-mediun" text="Contactar" />
                         </div>
                     </div>
                 </div>
+                )}
+                {selectedContact===2 &&
+                (
                 <div className="contacts-Instagram">
-
+                    <figure className="Instagram-name">
+                        <img src={nameInstagram} alt="icon nombre instagram" />
+                    </figure>
+                    <figure className="info-Photo infoPhotoInstagram">
+                        <img src={imgPersona} alt="Foto Maricarmen" />
+                    </figure>
+                    <div className="whatsapp-info_Name InfoNameInstagram">
+                        <p>Maricarmen Bohórquez Davila</p>
+                    </div>
+                    <div className="BtnContactInstagram" >
+                        <div className="btnMain">
+                            <BtnEnviar class="btn pink font-size-mediun" text="Contactar" />
+                        </div>
+                    </div>
                 </div>
+                )}
+                {selectedContact===3 &&
+                (
                 <div className="contacts-Faceboock">
-
-                </div>
+                    <figure className="Instagram-name">
+                       <img src={nameFaceboock} alt="icon nombre instagram" />
+                   </figure>
+                   <figure className="info-Photo infoPhotoInstagram">
+                       <img src={imgPersona} alt="Foto Maricarmen" />
+                   </figure>
+                   <div className="whatsapp-info_Name InfoNameInstagram">
+                       <p>Maricarmen Bohórquez Davila</p>
+                   </div>
+                   <div className="BtnContactInstagram" >
+                       <div className="btnMain">
+                           <BtnEnviar class="btn pink font-size-mediun" text="Contactar" />
+                       </div>
+                   </div>
+               </div>
+                )}
             </div>
         </section>
     )
